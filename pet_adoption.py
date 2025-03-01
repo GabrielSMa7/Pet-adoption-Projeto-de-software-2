de7107d20e98195ff685f598b61f9e0df75398b8
Gabriel <131276102+GabrielSMa7@users.noreply.github.com>
2025-03-01 15:27:12 -0300

    .

--- pet_adoption.py
+++ pet_adoption.py
@@ -4,13 +4,12 @@ from pet_adoption_plataform import shelter_profile
 from pet_adoption_plataform import forum
 import os
 
-
-
 while True:
     os.system("cls")
 
     if user_account.users != [""]:
-        print(f"Welcome {user_account.users[user].name} to our Pet Adoption Platform!!!")
+        print(f"{user_account.users}")
+        print(f"Welcome {user_account.users.name} to our Pet Adoption Platform!!!")
     else:
         print(f"Welcome quest to our Pet Adoption Platform!!!")
     print("--View and adopt our pets type (1)\n--View your profile type (2)\n--See shelters and rescue information type (3)\n--Forum (4)\n--Exit (5)")
